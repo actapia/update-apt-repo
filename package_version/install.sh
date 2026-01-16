c422c0f1f74cbbcb28da38b4384b12f077de3a32
actapia <andrew.tapia@uky.edu>
2026-01-16 18:40:44 -0500

    Fix package_version install script.

--- package_version/install.sh
+++ package_version/install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [[ -v DESTDIR ]]; then
+if ! [[ -v DESTDIR ]]; then
     DESTDIR="$HOME/.local/bin"
 fi
+mkdir -p "$DESTDIR"
 cp "$DIR"/{package_version.sh,get_changes_filenames.awk} \
    "$DIR"/_package_version_completions.sh "$DESTDIR"
 read -p "Source tab completions in ~/.bashrc?" -n 1 -r
